aff76bdd62683e6318890efe8e248fbcd0623eb2
Romario Melo <romario.melos@gmail.com>
2019-11-07 17:06:07 -0300

    principal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,21 +1,44 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import {
     SafeAreaView,
     ImageBackground,
     StyleSheet,
     Image,
-    Text,
     View,
-    ScrollView
+    StatusBar,
+    TouchableOpacity,
+    ActivityIndicator
 } from 'react-native'
 
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { LivePlayer } from "react-native-live-stream"
+
 const Main = ({navigation}) => {
+
+    const [stoped, setStop] = useState(true)
+    const [loading, setLoading] = useState(false)
+
+    const player = useRef(null)
+
     useEffect(()=> {
-        navigation.openDrawer()
+        console.log('didMount')
     }, [])
+
+    const btnControlPress = () => {
+        if (stoped) {
+            setLoading(true)
+            setStop(!stoped)
+            setTimeout(()=> {
+                setLoading(false)
+            },2000)
+        } else {
+            setStop(!stoped)
+        }
+    }
 
     return (
         <View>
+            <StatusBar backgroundColor="#ffffff" barStyle="dark-content"  />
         <SafeAreaView>
             <ImageBackground
                 source={require('../../assets/background.png')}
@@ -36,8 +59,40 @@ const Main = ({navigation}) => {
             </ImageBackground>
             
         </SafeAreaView>
-        <View style={styles.control}>
-            <View>
+        <View style={styles.controlContainer}>
+            <View style={styles.control}>
+            </View>
+            <View style={styles.btnContainer}>
+                {
+                    (loading) ?
+                        <ActivityIndicator size="large" color="#ffffff" style={styles.loader} /> :
+                        (stoped) ?
+                        <TouchableOpacity onPress={btnControlPress}>
+                            <Icon name="play-arrow" size={110} color="#fff" style={styles.btnPlay} />
+                        </TouchableOpacity> :
+                        <TouchableOpacity onPress={btnControlPress}>
+                            <Icon name="stop" size={110} color="#fff" style={styles.btnStop} />
+                        </TouchableOpacity>
+                }
+
+
+                { 
+                    (!stoped) ?
+                        <LivePlayer source={{uri:"http://radio.novatimbaubafm.com:8204/live"}}
+                            ref={player}
+                            paused={false}
+                            muted={false}
+                            bufferTime={500}
+                            maxBufferTime={1000}
+                            resizeMode={"contain"}
+                            onLoading={()=>{console.log("OnLoading")}}
+                            onLoad={()=>{console.log("OnLoad")}}
+                            onEnd={()=>{console.log("OnEnd")}}
+                            onProgress={() => {console.log("OnProgress")}}
+                        /> :
+                        null
+                }
+                
             </View>
         </View>
     </View>
@@ -46,7 +101,7 @@ const Main = ({navigation}) => {
 
 Main.navigationOptions = () => {
     return {
-        drawerLabel: 'Horário Escolar'
+        drawerLabel: 'Nova Timbaúba FM'
     }
 }
 
@@ -55,13 +110,37 @@ const styles = StyleSheet.create({
         alignItems: "center",
         height: "100%"
     },
-    control: {
-        backgroundColor: "#9fbb32",
+    controlContainer: {
         width: "100%",
         height: 150,
         position: "absolute",
         bottom: 0,
         left:0,
+        alignItems: "center"
+    },
+    control: {
+        backgroundColor: "#9fbb32",
+        width: "100%",
+        height: 100,
+        position: "absolute",
+        bottom: 0
+        
+    },
+    btnContainer: {
+        width: 126,
+        height: 126,
+        backgroundColor: "blue",
+        borderRadius: 63,
+        alignItems: "center"
+    },
+    btnStop: {
+        marginTop: 8
+    },
+    btnPlay: {
+        marginTop: 6
+    },
+    loader: {
+        marginTop: 43
     }
 })
 
